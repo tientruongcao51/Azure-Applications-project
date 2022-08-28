78db59cd543b7d6aa950f17e0bdba35a2e9ed188
truongct <tientruongcao51@gmail.com>
2022-08-28 22:20:25 +0700

    update user logs

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -22,10 +22,7 @@ imageSourceUrl = 'https://' + app.config['BLOB_ACCOUNT'] + '.blob.core.windows.n
 @login_required
 def home():
     user = User.query.filter_by(username=current_user.username).first_or_404()
-    app.logger.info('Login success home by user')
     app.logger.warning('Login success home by user')
-    app.logger.error('Login success home by user')
-    app.logger.critical('Login success home by user')
     posts = Post.query.all()
     return render_template(
         'index.html',
